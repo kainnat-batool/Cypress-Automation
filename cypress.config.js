436ecb55a9f10efab5f539f90d96df0e56c10914
Kainnat Batool <kainnatbatool09@gmail.com>
2024-01-17 11:03:25 +0500

    Add incognito Tab Configurations

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,20 @@ module.exports = defineConfig({
     baseUrl:'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+            launchOptions.args.push("--incognito");                
+            return launchOptions
+        }
+
+        if (browser.name === 'electron') {                
+            launchOptions.preferences.incognito = true               
+            return launchOptions
+        }
+    })    
       // implement node event listeners here they will run before any test case
     },
+    
   },
     env:{
       URL:'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
